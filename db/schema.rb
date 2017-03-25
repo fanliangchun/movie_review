719688d74858046503eb4d454d4f1deaf9185b66
fanliangchun <jeremydo2010@gmail.com>
2017-03-26 07:08:40 +0800

    add nickname

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325213523) do
+ActiveRecord::Schema.define(version: 20170325225656) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 20170325213523) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
