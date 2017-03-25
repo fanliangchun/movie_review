681f76edcbdbf02baaa0c8fb519643b3164b56e8
fanliangchun <jeremydo2010@gmail.com>
2017-03-25 11:44:32 +0800

    implement movie CURD

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,55 @@
+class MoviesController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
+	before_action :find_movie, only: [:edit, :update, :destroy]
+
+	def index
+		@movies = Movie.all
+	end
+
+	def show
+		@movie = Movie.find(params[:id])
+	end
+
+	def new
+		@movie = Movie.new
+	end
+
+	def create
+		@movie = Movie.new(movie_params)
+		@movie.user = current_user
+		if @movie.save
+			redirect_to movies_path, notice: "Created Successfully!"
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @movie.update(movie_params)
+			redirect_to movie_path, notice: "Updated Successfully!"
+		end
+	end
+
+	def destroy
+		@movie.destroy
+		redirect_to root_path, warning: "Moive Deleted!"
+	end
+
+	private
+
+	def find_movie
+		@movie = Movie.find(params[:id])
+
+		if current_user != @movie.user
+			flash[:alert] = "You have no permission!"
+			redirect_to root_path
+		end
+	end
+
+	def movie_params
+		params.require(:movie).permit(:title, :description, :director, :movie_length)
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325022552) do
+ActiveRecord::Schema.define(version: 20170325024143) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "director"
+    t.string   "movie_length"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
