83e7daca0c331cd19c38a747a6f9293ea6a1b8e9
fanliangchun <jeremydo2010@gmail.com>
2017-03-25 19:13:03 +0800

    add favorite show

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,6 @@ class Movie < ApplicationRecord
 	has_many :movie_relationships
 	has_many :members, :through => :movie_relationships, :source => :user
 	validates :title, presence: true
-	has_attached_file :avatar, styles: { medium: "300x450>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "300x450>", thumb: "200x200#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
